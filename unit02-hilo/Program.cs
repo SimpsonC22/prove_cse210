69695b936ed9c828ea4f67570bbaeeb79a874a9d
SimpsonC22 <sim20012@byui.edu>
2022-07-12 15:35:05 -0600

    Hilo update

--- unit02-hilo/Program.cs
+++ unit02-hilo/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using unit02_hilo.Classes;
 
 namespace unit02_hilo
 {
+    /// <sumary>
+    /// 
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GamePlay new_game = new GamePlay();
+            new_game.StartGame();
         }
     }
 }
